1473e75b942b832a472ba2b35d632ca41f097646
Emmytobs <46114276+Emmytobs@users.noreply.github.com>
2019-08-17 15:31:44 +0100

    Update Bread Price Calculator.js

--- Bread Price Calculator.js
+++ Bread Price Calculator.js
@@ -3,88 +3,6 @@
 */
 
 "use strict";
-/* 
-This function doesn't work. It doesn't multiply the quantity of bread with the price of that bread
-
-let breadType = document.querySelector("#type");
-let getPriceBtn = document.querySelector("#price");
-let showPriceField = document.querySelector("#show-price");
-
-function getBreadPrice(type) {
-  var breadQuantity = document.querySelector("#quantity");
-  var output;
-  if (type == "filade") {
-    output = breadQuantity.value * 190;
-  }
-  return output;
-}
-
-var priceOutput = getBreadPrice(breadType.value);
-getPriceBtn.addEventListener("click", function() {
-  console.log(priceOutput);
-});
- */
-
-/* But this function works just fine */
-
-
-
-
-
-
-
-
-
-
-
-
-// let getPriceBtn = document.querySelector("#price");
-
-
-
-// getPriceBtn.addEventListener("click", function() {
-//   let breadType = document.querySelector("#type");
-//   let breadQuantity = document.querySelector("#quantity");
-//   let showPriceField = document.querySelector("#show-price");
-
-//   //
-//   //
-//   if (breadType.value == "filade") {
-//         let newEl = document.createElement('LI');
-//         let string = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`
-//         let newElContent = document.createTextNode( string );
-//         newEl.appendChild(newElContent);
-//         showPriceField.appendChild(newEl);
-//         // showPriceField.innerHTML = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`;
-//   }
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-let getPriceBtn = document.querySelector("#price");
-
-getPriceBtn.addEventListener("click", function() {
-    let breadType = document.querySelector("#type");
-    let breadQuantity = document.querySelector("#quantity");
-    let showPriceField = document.querySelector("#show-price");
-  if (breadType.value == "filade") {
-        showPriceField.innerHTML = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`;
-  }
-});
- */
- 
-
 
 
 
@@ -144,13 +62,6 @@ getPriceBtn.addEventListener('click', () => console.log("clicked"));
 
 
 function calculateBreadPrice() {
-  // let newEl = document.createElement('LI');
-  //       let string = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`
-  //       let newElContent = document.createTextNode( string );
-  //       newEl.appendChild(newElContent);
-  //       showPriceField.appendChild(newEl);
-        // showPriceField.innerHTML = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`;
-
     for ( let breadListIdx = 0; breadListIdx < breadList.length; breadListIdx++ ) {
 
        for ( let breadName in breadList[breadListIdx]  ) {
@@ -162,20 +73,9 @@ function calculateBreadPrice() {
                 let newElContent = document.createTextNode( string );
                 newEl.appendChild(newElContent);
                 showPriceField.appendChild(newEl);
-                // showPriceField.innerHTML = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`;
                 return showPriceField;
-                
-
-
-                // console.log(breadList[breadListIdx][breadName]);
-                // console.log(breadList[breadListIdx][breadName][sizeProperty]);
-                // console.log(`${breadQuantity.value} ${breadName} (${breadSize.value}) will cost ${breadQuantity.value * breadList[breadListIdx][breadName][sizeProperty] } `);
+                             
               }
-
-
-
-
-
           }
         }
     }
@@ -185,42 +85,4 @@ function calculateBreadPrice() {
 
 //Call the function on a click event to the "get price" button.
 getPriceBtn.addEventListener("click", calculateBreadPrice);
-
-
-// for ( let breadListIdx = 0; breadListIdx < breadList.length; breadListIdx++ ) {
-
-//    for ( let breadName in breadList[breadListIdx]  ) {
-//       for (let sizeProperty in breadList[breadListIdx][breadName]) {
-//           if (breadType.value == breadName && breadQuantity.value == sizeProperty ) {
-//             console.log(breadList[breadListIdx][breadName]);
-//             console.log(breadList[breadListIdx][breadName][sizeProperty]);
-//           }
-
-
-
-
-
-//       }
-    
-
-    // for (prop in breadList[breadListIdx][breadName]) {
-    //   if (breadQuantity.value == prop ) {
-    //     console.log(prop);
-    //   }
-    // }
-    
-    // console.log(`${breadName} =>>> ${breadName[0]}`);
-
-
-
-  //
-  //
-  // if (breadType.value == "filade") {
-  //       let newEl = document.createElement('LI');
-  //       let string = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`
-  //       let newElContent = document.createTextNode( string );
-  //       newEl.appendChild(newElContent);
-  //       showPriceField.appendChild(newEl);
-  //       // showPriceField.innerHTML = `${breadQuantity.value} filade will cost -N- ${breadQuantity.value * 190}`;
-  // }
 
